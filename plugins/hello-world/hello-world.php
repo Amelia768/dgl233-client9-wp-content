dfad7c37ba994ce4f772875091fc9031fbe74123
Amelia768 <71101378+Amelia768@users.noreply.github.com>
2022-04-07 20:10:36 -0700

    Create Hello World plugin

---
+++ plugins/hello-world/hello-world.php
@@ -0,0 +1,21 @@
+<?php 
+/** 
+ * Hello World 
+ * 
+ * @package HelloWorld 
+ * @author Your Name
+ * @copyright 2020 Your Name 
+ * @license GPL-2.0-or-later 
+ * 
+ * @wordpress-plugin 
+ * Plugin Name: Hello World 
+ * Plugin URI: https://mysite.com/hello-world 
+ * Description: Prints "Hello World" in WordPress admin. 
+ * Version: 0.0.1 
+ * Author: Your Name 
+ * Author URI: https://mysite.com 
+ * Text Domain: hello-world 
+ * License: GPL v2 or later 
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt */
+...
+?>
